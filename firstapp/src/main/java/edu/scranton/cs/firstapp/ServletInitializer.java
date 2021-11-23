ca625d98c99b596f3188413e01aea477139453c8
espositoe4 <evan.esposito@scranton.edu>
2021-11-23 16:11:41 -0500

    initial commit

---
+++ firstapp/src/main/java/edu/scranton/cs/firstapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.scranton.cs.firstapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstappApplication.class);
+	}
+
+}
